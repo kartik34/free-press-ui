ff4d2f595ff8205a4e6a85da2925b1520ca76002
Kartik Singhal <kartiksinghal3.14@gmail.com>
2022-01-16 17:15:04 -0500

    Break down app into subcomponents

---
+++ src/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+import 'bulma/css/bulma.min.css';
+
+function Navbar(props: any) {
+    return (
+        <div>
+            <img
+                id="logo"
+                src="https://i.imgur.com/VwNdeME.png"
+                height="30"
+            />
+        </div>
+    );
+}
+
+export default Navbar;
+
+
